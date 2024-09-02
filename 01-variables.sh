0be199715deaa069bff6d9eea53bc5d1db32c33d
“yamuna <grvyamunasree1320@gmail.com>
2024-09-03 00:45:01 +0530

    samplenotes.sh

---
+++ 01-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fruits=("Apple" "Berry" "jackfruit")
+
+echo "first fruit is $(fruits[0])"
+echo "second fruit is $(fruits[1])"
+echo "third fruit is $(fruits[2])"
+
+echo "read all fruits $(fruits[0])"
