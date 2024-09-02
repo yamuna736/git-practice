51a9e04fa13e5660534b6141b7ebccb9154532e0
“yamuna <grvyamunasree1320@gmail.com>
2024-09-03 00:06:06 +0530

    samplenotes.sh

--- samplenotes.sh
+++ samplenotes.sh
@@ -3,8 +3,8 @@
 PERSON1 = A
 PERSON2 = B
 
-echo $PERSON1:: Hello ${PERSON2}, Are u learning shell script?
-echo $PERSON2:: yes ${PERSON1}, started learning
-echo $PERSON1:: mr. ${PERSON2}, can you tell me the process of git! how we can use?
-echo $PERSON2:: sure mr.${PERSON1}, git is nothing but we need to add, commit, push
-echo $PERSON1:: Thank you! mr.${BPERSON2}.
+echo ${PERSON1}:: Hello ${PERSON2}, Are u learning shell script?
+echo ${PERSON2}:: yes ${PERSON1}, started learning
+echo ${PERSON1}:: mr. ${PERSON2}, can you tell me the process of git! how we can use?
+echo ${PERSON2}:: sure mr.${PERSON1}, git is nothing but we need to add, commit, push
+echo ${PERSON1}:: Thank you! mr.${PERSON2}.
