ba2aa9519b3c6e62d204dc63ac02b625e17c5808
“yamuna <grvyamunasree1320@gmail.com>
2024-09-03 23:29:48 +0530

    functions.sh

--- special-variables.sh
+++ special-variables.sh
@@ -3,3 +3,7 @@
 echo "All the variables passed: $@"
 echo "numner of args or varaibles passed: $#"
 echo " to know the running script: $0"
+echo "to know the current directory: $PWD"
+echo "to know the current user: $HOME"
+echo "present PID: $$"
+echo "background pid: $!"
