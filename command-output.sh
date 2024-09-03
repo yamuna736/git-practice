cf7f9f5fe884bcadea70cd15be9787fc01f52224
“yamuna <grvyamunasree1320@gmail.com>
2024-09-03 16:11:51 +0530

    functions.sh

--- command-output.sh
+++ command-output.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DATE=$(date)
+DATE=$(date + %F)
 
 echo "the date is: $DATE"
---
+++ datatypes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+sum =(($NUMBER1+$NUMBER2))
+
+echo "total is $sum"
