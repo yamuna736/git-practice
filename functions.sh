7fefce4973dc6652c0044a8c7f5f9941162e3425
“yamuna <grvyamunasree1320@gmail.com>
2024-09-05 00:28:42 +0530

    functions.sh

--- functions.sh
+++ functions.sh
@@ -2,24 +2,57 @@
 
 USERID=$(id -u)
 
+    #echo "user id is $USERID"
+
+    VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo "command is.. success"
+    else
+        echo "command is.. failed"
+
+    fi
+    }
+
+
 if [ $USERID -ne 0 ]
-then
-    echo "proceed with root previliges"
+then  
+    echo "proceed with  root previliges"
     exit 1
 fi 
-dnf intsall git -y
 
-if [ $? -ne 0 ]
-then
-    echo " git is installing"
-if [$! -ne 0 ]
-then 
-    echo "installed git is .. failed, check it"
-    exit 1
-else 
-echo " installed git is success"
-fi
-    else
-        echo " git is already installed"
-fi
-    dnf install git -y
+    dnf list installed git
+
+    VALIDATE $?
+
+# if [ $? -ne 0 ]
+# then
+#     echo " git is not installed, trying to install it.."
+#     dnf install git -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "git installation is not sucess... check it"
+#         exit 1
+#     else    
+#     echo " git installation is success.."
+#     fi
+# else
+#     echo " git is already installed, nothing to do.."
+# fi
+
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "mysql is not install.. try to install.."
+#     dnf install mysql -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo " mysql installation is not success.. check it"
+#         exit 1
+#     else
+#         echo "mysql already installed.."
+#     fi    
+# else
+#     echo "mysql is already install.. nothig to do"
+# fi
