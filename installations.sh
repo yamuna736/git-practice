7291d29e0f0949d73b1edae2aef67c3d81ff4c74
“yamuna <grvyamunasree1320@gmail.com>
2024-09-04 23:51:41 +0530

    functions.sh

--- installations.sh
+++ installations.sh
@@ -22,34 +22,34 @@ fi
 
 validate $? "listing the git"
 
-# if [ $? -ne 0 ]
-# then
-#     echo " git is not installed, trying to install it.."
-#     dnf install git -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "git installation is not sucess... check it"
-#         exit 1
-#     else    
-#     echo " git installation is success.."
-#     fi
-# else
-#     echo " git is already installed, nothing to do.."
-# fi
+if [ $? -ne 0 ]
+then
+    echo " git is not installed, trying to install it.."
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "git installation is not sucess... check it"
+        exit 1
+    else    
+    echo " git installation is success.."
+    fi
+else
+    echo " git is already installed, nothing to do.."
+fi
 
-# dnf list installed mysql
+dnf list installed mysql
 
-# if [ $? -ne 0 ]
-# then 
-#     echo "mysql is not install.. try to install.."
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo " mysql installation is not success.. check it"
-#         exit 1
-#     else
-#         echo "mysql already installed.."
-#     fi    
-# else
-#     echo "mysql is already install.. nothig to do"
-# fi
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not install.. try to install.."
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo " mysql installation is not success.. check it"
+        exit 1
+    else
+        echo "mysql already installed.."
+    fi    
+else
+    echo "mysql is already install.. nothig to do"
+fi
