d1bfb67a8ffd0ed348a29f57aafd2ded2e1b1ff4
“yamuna <grvyamunasree1320@gmail.com>
2024-09-04 17:57:56 +0530

    functions.sh

--- installations.sh
+++ installations.sh
@@ -16,12 +16,13 @@ if [ $? -ne 0 ]
 then
     echo " git is not installed, trying to install it.."
     dnf install git -y
-if [ $? -ne 0 ]
-then
-    echo "git installation is not sucess... check it"
-    exit 1
-else    
+    if [ $? -ne 0 ]
+    then
+        echo "git installation is not sucess... check it"
+        exit 1
+    else    
     echo " git installation is success.."
+    fi
 else
     echo " git is already installed, nothing to do.."
 fi
