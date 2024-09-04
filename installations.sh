c76d0fda6ec9480b58b3ba64d8b56a887407ee73
“yamuna <grvyamunasree1320@gmail.com>
2024-09-04 23:47:18 +0530

    functions.sh

--- installations.sh
+++ installations.sh
@@ -3,6 +3,14 @@
 USERID=$(id -u)
 
     #echo "user id is $USERID"
+validate(){
+    if [$1 -ne 0 ]
+    then
+        echo "$2 is.... Failed"
+        exit 1
+    else
+        echo "$2 is...Success"
+    fi
 
 if [ $USERID -ne 0 ]
 then  
@@ -10,36 +18,38 @@ then
     exit 1
 fi 
 
-dnf list installed git
+    dnf list installed git
 
-if [ $? -ne 0 ]
-then
-    echo " git is not installed, trying to install it.."
-    dnf install git -y
-    if [ $? -ne 0 ]
-    then
-        echo "git installation is not sucess... check it"
-        exit 1
-    else    
-    echo " git installation is success.."
-    fi
-else
-    echo " git is already installed, nothing to do.."
-fi
+validate $? "listing the git"
 
-dnf list installed mysql
+# if [ $? -ne 0 ]
+# then
+#     echo " git is not installed, trying to install it.."
+#     dnf install git -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "git installation is not sucess... check it"
+#         exit 1
+#     else    
+#     echo " git installation is success.."
+#     fi
+# else
+#     echo " git is already installed, nothing to do.."
+# fi
 
-if [ $? -ne 0 ]
-then 
-    echo "mysql is not install.. try to install.."
-    dnf install mysql -y
-    if [ $? -ne 0 ]
-    then
-        echo " mysql installation is not success.. check it"
-        exit 1
-    else
-        echo "mysql already installed.."
-    fi    
-else
-    echo "mysql is already install.. nothig to do"
-fi
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "mysql is not install.. try to install.."
+#     dnf install mysql -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo " mysql installation is not success.. check it"
+#         exit 1
+#     else
+#         echo "mysql already installed.."
+#     fi    
+# else
+#     echo "mysql is already install.. nothig to do"
+# fi
