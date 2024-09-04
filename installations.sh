c5525abdc0c95f795c5677d8d87980582750ae24
“yamuna <grvyamunasree1320@gmail.com>
2024-09-04 16:44:32 +0530

    functions.sh

--- installations.sh
+++ installations.sh
@@ -10,5 +10,13 @@ then
     exit 1
 fi 
 
-dnf install git -y
+dnf list installed git
+
+if [ $? -ne o ]
+then
+    echo " git is not installed, trying to install it.."
+    dnf install git -y
+else
+    echo " git is already installed, nothing to do.."
+fi
 
