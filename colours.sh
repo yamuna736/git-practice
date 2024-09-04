bfd0b208057fbc9b0dbd42d84021697c6f82cca3
“yamuna <grvyamunasree1320@gmail.com>
2024-09-05 01:05:49 +0530

    functions.sh

---
+++ colours.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+    #echo "user id is $USERID"
+    R="\e[31m"
+    G="\e[32m"
+    N="\e[33m"
+
+    VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is.. failed $R"
+        exit 1
+    else 
+        echo "$2 is.. success $G"
+
+
+    fi
+    }
+
+
+if [ $USERID -ne 0 ]
+then  
+    echo "proceed with  root previliges"
+    exit 1
+fi 
+
+    dnf list installed git
+
+    
+if [ $? -ne 0 ]
+then
+    echo " git is not installed, trying to install it.."
+    dnf install git -y
+    VALIDATE $? "installing git"
+        exit 1
+    
+else
+    echo " git is already installed, nothing to do.."
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not install.. try to install.."
+    dnf install mysql -y
+    VALIDATE $? "installing mysql"
+     
+else
+    echo "mysql is already install.. nothig to do"
+fi
