05c44217f53e06a800be097be246979a1310fc65
“yamuna <grvyamunasree1320@gmail.com>
2024-09-05 15:04:37 +0530

    functions.sh

--- practicenotes.sh
+++ practicenotes.sh
@@ -1,14 +1,21 @@
 #!?bin/bash
 
-NUMBER1=$1
-NUMBER2=$2
+ NUMBER1=$1
+# NUMBER2=$2
 
-sum=$(($NUMBER1+$NUMBER1))
-subtract=$(($NUMBER1-$NUMBER2))
-multipy=$(($NUMBER1^$NUMBER2))
+# sum=$(($NUMBER1+$NUMBER1))
+# subtract=$(($NUMBER1-$NUMBER2))
 
 
-echo "the total sumof 2 number is : $sum"
-echo "the total sumof 2 number is : $subtract"
-echo "the total sumof 2 number is : $multiply"
+
+# echo "the total sumof 2 number is : $sum"
+# echo "the total sumof 2 number is : $subtract"
+
+
+if [ $NUMBER1 -gt 20]
+then 
+    echo " given number is: $NUMBER1 is greater than 20"
+    else
+        echo " given number is: $NUMBER1 is less than 20"
+fi
 
