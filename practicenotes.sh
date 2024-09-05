e29025a441fc492f1f5b6e9f5c91ca35d1180708
“yamuna <grvyamunasree1320@gmail.com>
2024-09-05 14:53:15 +0530

    functions.sh

--- practicenotes.sh
+++ practicenotes.sh
@@ -4,6 +4,11 @@ NUMBER1=$1
 NUMBER2=$2
 
 sum=$(($NUMBER1+$NUMBER1))
+subtract=$(($NUMBER1-$NUMBER2))
+multipy=$(($NUMBER1*$NUMBER2))
+
 
 echo "the total sumof 2 number is : $sum"
+echo "the total sumof 2 number is : $subtract"
+echo "the total sumof 2 number is : $multiply"
 
