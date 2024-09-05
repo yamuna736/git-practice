2a2b70397329becd9193d15082bc13bdc96ac037
“yamuna <grvyamunasree1320@gmail.com>
2024-09-05 14:54:01 +0530

    functions.sh

--- practicenotes.sh
+++ practicenotes.sh
@@ -5,7 +5,7 @@ NUMBER2=$2
 
 sum=$(($NUMBER1+$NUMBER1))
 subtract=$(($NUMBER1-$NUMBER2))
-multipy=$(($NUMBER1*$NUMBER2))
+multipy=$(($NUMBER1^$NUMBER2))
 
 
 echo "the total sumof 2 number is : $sum"
