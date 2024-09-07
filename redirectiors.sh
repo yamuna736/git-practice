e9485eacd4274a3c33846bb3710b24d26f854cf6
“yamuna <grvyamunasree1320@gmail.com>
2024-09-07 20:15:25 +0530

    functions.sh

--- redirectiors.sh
+++ redirectiors.sh
@@ -4,7 +4,7 @@ LOGS_FOLDER="var/logs/shell-script"
 SCRIPT_NAME=$(echo "0" | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
-mkdir -p $LOGS_FOLDER
+sudo mkdir -p $LOGS_FOLDER
 
 
 USERID=$(id -u)
